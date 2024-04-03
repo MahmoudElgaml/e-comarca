4908f571fa4bebd3c141a889d9add004c0c3be90
Owner <mahmoudelgmal991@gmail.com>
2024-04-03 04:44:01 +0200

    adds logo in login

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages{
+  static const String login_logo="assets/images/log_in_photo.png";
+  static const String bubbleText="assets/images/bubble_text.png";
+
+}
--- lib/features/log_in/presentation/pages/login_screen.dart
+++ lib/features/log_in/presentation/pages/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:e_comarce_clean/core/utils/app_color.dart';
+import 'package:e_comarce_clean/core/utils/app_images.dart';
 import 'package:e_comarce_clean/core/utils/app_style.dart';
 import 'package:e_comarce_clean/features/log_in/presentation/widgets/login_drawer.dart';
 import 'package:flutter/cupertino.dart';
@@ -18,11 +19,24 @@ class LoginScreen extends StatelessWidget {
           Container(
             color: AppColor.authColor,
             height: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 88.h, left: 36.w),
+                  child: Image.asset(AppImages.bubbleText),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40.h),
+                  child: Image.asset(AppImages.login_logo),
+                ),
+              ],
+            ),
           ),
-         const LogInDrawer(),
+          const LogInDrawer(),
         ],
       ),
     );
   }
 }
-
