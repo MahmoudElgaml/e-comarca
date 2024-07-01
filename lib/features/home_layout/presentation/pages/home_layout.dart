99fa0ab999cb0d20cb28cf7861f163a32f5433a5
Elgaml <mahmoudelgmal991@gmail.com>
2024-07-02 02:36:50 +0300

    finish bottomNavigation design

--- lib/features/home_layout/presentation/pages/home_layout.dart
+++ lib/features/home_layout/presentation/pages/home_layout.dart
@@ -1,5 +1,9 @@
+import 'package:e_comarce_clean/core/utils/app_color.dart';
 import 'package:e_comarce_clean/features/home/presentation/pages/home_feature_screen.dart';
+import 'package:e_comarce_clean/features/home_layout/presentation/widgets/bottomnavi_item.dart';
+import 'package:e_comarce_clean/generated/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../home/presentation/widgets/costume_appbar.dart';
 
@@ -8,15 +12,42 @@ class HomeLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
-       body: Padding(
-         padding: EdgeInsets.only(top: 6,left: 16,right: 16),
-         child: HomeScreen(),
-       ),
-
-      ),
+          backgroundColor: Colors.white,
+          body: const Padding(
+            padding: EdgeInsets.only(top: 6, left: 16, right: 16),
+            child: HomeScreen(),
+          ),
+          bottomNavigationBar: ClipRRect(
+             borderRadius: const BorderRadius.only(topRight: Radius.circular(15),topLeft: Radius.circular(15)),
+            child: BottomNavigationBar(
+               showSelectedLabels: false,
+              items: const [
+                BottomNavigationBarItem(
+                    backgroundColor: AppColor.primaryColor,
+                    icon: BottomNaviItem(
+                      icon: Assets.imagesHomeIcon,
+                    ),
+                    label: "."),
+                BottomNavigationBarItem(
+                    icon: BottomNaviItem(
+                      icon: Assets.imagesCategoryIcon,
+                    ),
+                    label: "."),
+                BottomNavigationBarItem(
+                    icon: BottomNaviItem(
+                      icon: Assets.imagesWichList,
+                    ),
+                    label: "."),
+                BottomNavigationBarItem(
+                    icon: BottomNaviItem(
+                      icon: Assets.imagesAccountIcon,
+                    ),
+                    label: "."),
+              ],
+            ),
+          )),
     );
   }
 }
