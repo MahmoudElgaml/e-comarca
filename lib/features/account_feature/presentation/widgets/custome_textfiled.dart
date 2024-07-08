ed00253a962e8f0b2270a6e1f2d94f0283a5d456
Elgaml <mahmoudelgmal991@gmail.com>
2024-07-08 19:40:26 +0300

    add edit textFiled feature

--- lib/features/account_feature/presentation/widgets/custome_textfiled.dart
+++ lib/features/account_feature/presentation/widgets/custome_textfiled.dart
@@ -2,11 +2,18 @@ import 'package:e_comarce_clean/core/utils/app_style.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class CustomTextFiled extends StatelessWidget {
+class CustomTextFiled extends StatefulWidget {
   const CustomTextFiled({super.key, required this.title, required this.hint});
 
   final String title;
   final String hint;
+
+  @override
+  State<CustomTextFiled> createState() => _CustomTextFiledState();
+}
+
+class _CustomTextFiledState extends State<CustomTextFiled> {
+  bool isEnable = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,24 +21,50 @@ class CustomTextFiled extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          title,
+          widget.title,
           style: AppStyle.style18(context),
         ),
         const Gap(16),
-        TextField(
-          decoration: InputDecoration(
-              hintText: hint,
-              hintStyle: AppStyle.style14(context),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1,
-                  color: Color(0x4C004182),
-                ),
-              )),
+        Stack(
+          alignment: AlignmentDirectional.centerEnd,
+          children: [
+            TextField(
+              onTapOutside: (event) {
+                isEnable = false;
+                setState(() {});
+              },
+              enabled: isEnable,
+              decoration: InputDecoration(
+                hintText: widget.hint,
+                hintStyle: AppStyle.style14(context),
+                enabledBorder: buildTextFiledBorder(),
+                disabledBorder: buildTextFiledBorder(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: () {
+                  isEnable = true;
+                  setState(() {});
+                },
+                icon: const Icon(Icons.edit, color: Colors.grey),
+              ),
+            ),
+          ],
         ),
         const Gap(24),
       ],
+    );
+  }
+
+  OutlineInputBorder buildTextFiledBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(15),
+      borderSide: const BorderSide(
+        width: 1,
+        color: Color(0x4C004182),
+      ),
     );
   }
 }
