4f83f743c55f12ab21b35c879e004468c354f0ef
Elgaml <mahmoudelgmal991@gmail.com>
2024-07-18 16:05:00 +0300

    cart screen getCartFeature: create cartProductModel and make it Compatible with entity

---
+++ lib/features/cart_feature/data/models/CartProductsModel.dart
@@ -0,0 +1,217 @@
+import 'package:e_comarce_clean/features/cart_feature/domain/entities/CartProduct.dart';
+
+class CartProductsModel {
+  CartProductsModel({
+    this.status,
+    this.numOfCartItems,
+    this.data,
+  });
+
+  CartProductsModel.fromJson(dynamic json) {
+    status = json['status'];
+    numOfCartItems = json['numOfCartItems'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+
+  String? status;
+  num? numOfCartItems;
+  Data? data;
+}
+
+class Data extends CartDataEntity {
+  Data({
+    super.id,
+    super.cartOwner,
+    super.products,
+    this.createdAt,
+    this.updatedAt,
+    this.v,
+    this.totalCartPrice,
+  });
+
+  Data.fromJson(dynamic json) {
+    id = json['_id'];
+    cartOwner = json['cartOwner'];
+    if (json['products'] != null) {
+      products = [];
+      json['products'].forEach((v) {
+        products?.add(Products.fromJson(v));
+      });
+    }
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    v = json['__v'];
+    totalCartPrice = json['totalCartPrice'];
+  }
+
+  String? createdAt;
+  String? updatedAt;
+  num? v;
+  num? totalCartPrice;
+}
+
+class Products extends ProductsData {
+  Products({
+    super.count,
+    super.id,
+    super.product,
+    super.price,
+  });
+
+  Products.fromJson(dynamic json) {
+    count = json['count'];
+    id = json['_id'];
+    product =
+        json['product'] != null ? Product.fromJson(json['product']) : null;
+    price = json['price'];
+  }
+}
+
+class Product extends ProductData {
+  Product({
+    this.subcategory,
+    super.id,
+    super.title,
+    super.quantity,
+    super.imageCover,
+    this.category,
+    this.brand,
+    super.ratingsAverage,
+    this.id2,
+  });
+
+  Product.fromJson(dynamic json) {
+    if (json['subcategory'] != null) {
+      subcategory = [];
+      json['subcategory'].forEach((v) {
+        subcategory?.add(Subcategory.fromJson(v));
+      });
+    }
+    id = json['_id'];
+    title = json['title'];
+    quantity = json['quantity'];
+    imageCover = json['imageCover'];
+    category =
+        json['category'] != null ? Category.fromJson(json['category']) : null;
+    brand = json['brand'] != null ? Brand.fromJson(json['brand']) : null;
+    ratingsAverage = json['ratingsAverage'];
+    id2 = json['id'];
+  }
+
+  List<Subcategory>? subcategory;
+
+  Category? category;
+  Brand? brand;
+
+  String? id2;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (subcategory != null) {
+      map['subcategory'] = subcategory?.map((v) => v.toJson()).toList();
+    }
+    map['_id'] = id;
+    map['title'] = title;
+    map['quantity'] = quantity;
+    map['imageCover'] = imageCover;
+    if (category != null) {
+      map['category'] = category?.toJson();
+    }
+    if (brand != null) {
+      map['brand'] = brand?.toJson();
+    }
+    map['ratingsAverage'] = ratingsAverage;
+    map['id'] = id;
+    return map;
+  }
+}
+
+class Brand {
+  Brand({
+    this.id,
+    this.name,
+    this.slug,
+    this.image,
+  });
+
+  Brand.fromJson(dynamic json) {
+    id = json['_id'];
+    name = json['name'];
+    slug = json['slug'];
+    image = json['image'];
+  }
+
+  String? id;
+  String? name;
+  String? slug;
+  String? image;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['_id'] = id;
+    map['name'] = name;
+    map['slug'] = slug;
+    map['image'] = image;
+    return map;
+  }
+}
+
+class Category {
+  Category({
+    this.id,
+    this.name,
+    this.slug,
+    this.image,
+  });
+
+  Category.fromJson(dynamic json) {
+    id = json['_id'];
+    name = json['name'];
+    slug = json['slug'];
+    image = json['image'];
+  }
+
+  String? id;
+  String? name;
+  String? slug;
+  String? image;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['_id'] = id;
+    map['name'] = name;
+    map['slug'] = slug;
+    map['image'] = image;
+    return map;
+  }
+}
+
+class Subcategory {
+  Subcategory({
+    this.id,
+    this.name,
+    this.slug,
+    this.category,
+  });
+
+  Subcategory.fromJson(dynamic json) {
+    id = json['_id'];
+    name = json['name'];
+    slug = json['slug'];
+    category = json['category'];
+  }
+
+  String? id;
+  String? name;
+  String? slug;
+  String? category;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['_id'] = id;
+    map['name'] = name;
+    map['slug'] = slug;
+    map['category'] = category;
+    return map;
+  }
+}
