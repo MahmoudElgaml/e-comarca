358e40571958759c9daa8dcb6bd9a73d3823f447
Elgaml <mahmoudelgmal991@gmail.com>
2024-07-23 05:03:48 +0300

    cart screen : add total price widget to cart screen

---
+++ lib/features/cart_feature/presentation/widgets/totalprice_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../config/routes/routes.dart';
+import '../../../../core/utils/app_color.dart';
+import '../../../../core/utils/app_style.dart';
+
+class TotalPriceWidget extends StatelessWidget {
+  const TotalPriceWidget({super.key,required this.totalPrice});
+ final String totalPrice;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(
+              "Total Price",
+              style: AppStyle.style18(context).copyWith(
+                color: const Color(0x9906004E),
+              ),
+            ),
+            const Gap(12),
+            Text(
+              totalPrice,
+              style: AppStyle.style18(context),
+            ),
+          ],
+        ),
+        const Gap(33),
+        Expanded(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: AppColor.primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20))),
+            onPressed: () {
+              context.push(AppRoute.cartScreen);
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              child: FittedBox(
+                child: Row(
+                  children: [
+
+
+                    Center(
+                      child: Text(
+                        "Check out ->",
+                        style: AppStyle.styleMedium20(context)
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
