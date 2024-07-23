b96ca76a432646d186ce5befcacf5f9649ffbeb9
Elgaml <mahmoudelgmal991@gmail.com>
2024-07-23 05:03:48 +0300

    cart screen : add total price widget to cart screen

--- lib/features/cart_feature/presentation/pages/cart_screen.dart
+++ lib/features/cart_feature/presentation/pages/cart_screen.dart
@@ -5,6 +5,8 @@ import 'package:e_comarce_clean/core/utils/cpmponents/product_cart.dart';
 import 'package:e_comarce_clean/features/cart_feature/data/models/CartProductsModel.dart';
 import 'package:e_comarce_clean/features/cart_feature/domain/entities/CartProduct.dart';
 import 'package:e_comarce_clean/features/cart_feature/presentation/manager/get_cart_product_cubit.dart';
+import 'package:e_comarce_clean/features/cart_feature/presentation/widgets/totalprice_widget.dart';
+import 'package:e_comarce_clean/features/product_detail_feature/presentation/widgets/price_addtocart_widget.dart';
 import 'package:e_comarce_clean/features/products_feature/presentation/manager/get_product_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -43,51 +45,53 @@ class CartScreen extends StatelessWidget {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 32, left: 16, right: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<GetCartProductCubit, GetCartProductState>(
-              builder: (context, state) {
-                if (state is GetCartProductUnLoggedState) {
-                  return const NoLoggedWidget();
-                } else if (state is GetCartProductEmptyState) {
-                  return const Center(
-                    child: Text(
-                      "no data go do some shopping",
-                    ),
-                  );
-                } else if (state is GetCartProductFailState) {
-                  return Center(
-                    child: Text(
-                      state.message,
-                    ),
-                  );
-                } else if (state is GetCartProductSuccessState) {
-                  List<CartProducts> products = GetCartProductCubit
-                      .get(context)
-                      .cartData!
-                      .data!
-                      .products ??
-                      [];
-                  return Expanded(
+      body: BlocBuilder<GetCartProductCubit, GetCartProductState>(
+        builder: (context, state) {
+          List<CartProducts>? products =
+              GetCartProductCubit.get(context).cartData?.data?.products ?? [];
+          if (state is GetCartProductUnLoggedState) {
+            return const NoLoggedWidget();
+          } else if (state is GetCartProductEmptyState) {
+            return const Center(
+              child: Text(
+                "no data go do some shopping",
+              ),
+            );
+          } else if (state is GetCartProductFailState) {
+            return Center(
+              child: Text(
+                state.message,
+              ),
+            );
+          } else if (state is GetCartProductSuccessState) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 32, left: 16, right: 16,bottom: 32),
+              child: Column(
+                children: [
+                  Expanded(
                     child: ListView.separated(
-                      itemBuilder: (context, index) =>
-                          ProductCart(
-                            product: products[index],
-                            isCart: true,
-                          ),
+                      itemBuilder: (context, index) => ProductCart(
+                        product: products[index],
+                        isCart: true,
+                      ),
                       separatorBuilder: (context, index) => const Gap(20),
                       itemCount: products.length,
                     ),
-                  );
-                }
-                return const Center(child: CircularProgressIndicator());
-              },
-            ),
-          ],
-        ),
+                  ),
+
+                  TotalPriceWidget(
+                      totalPrice: GetCartProductCubit.get(context)
+                              .cartData
+                              ?.data
+                              ?.totalCartPrice
+                              .toString() ??
+                          "")
+                ],
+              ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
