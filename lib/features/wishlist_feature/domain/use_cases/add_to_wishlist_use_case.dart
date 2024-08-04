efa0dd1e08b41e2651b6b592967b55337d76e482
Elgaml <mahmoudelgmal991@gmail.com>
2024-08-04 14:37:07 +0300

    wishlist screen (add to wish list feature): finish domain layer

---
+++ lib/features/wishlist_feature/domain/use_cases/add_to_wishlist_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_comarce_clean/features/wishlist_feature/domain/repositories/wishlist_repo.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../../core/erorr/failure.dart';
+
+@injectable
+class AddToWishlistUseCase{
+  WishlistRepo  wishlistRepo;
+
+  AddToWishlistUseCase(this.wishlistRepo);
+
+  Future<Either<Failure,String>>call()async{
+    return await wishlistRepo.addToWishlist();
+
+
+  }
+}
