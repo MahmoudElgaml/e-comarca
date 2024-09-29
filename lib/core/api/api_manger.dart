d8196a4daa1ed7a49cec41d5555cbaa5b73279ea
Elgaml <mahmoudelgmal991@gmail.com>
2024-09-29 22:36:27 +0300

    -change the Dio instance to Dio Factory - add PrettyLogger Package

--- lib/core/api/api_manger.dart
+++ lib/core/api/api_manger.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:e_comarce_clean/core/api/dio_factory.dart';
 import 'package:e_comarce_clean/core/api/end_points.dart';
 import 'package:e_comarce_clean/core/cache/storage_token.dart';
 import 'package:injectable/injectable.dart';
@@ -6,10 +7,20 @@ import 'package:injectable/injectable.dart';
 @singleton
 @injectable
 class APiManger {
-  final Dio dio = Dio();
+  final Dio dio = DioFactory.getDio();
 
   APiManger();
 
+
+
+
+
+
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////
   Future<Response> get(String endPoint, {Map<String, dynamic>? header}) {
     return dio.get(EndPoints.baseUrl + endPoint,
         options: Options(headers: header, preserveHeaderCase: true));
@@ -18,13 +29,17 @@ class APiManger {
   Future<Response> post(String endPoint, Map<String, dynamic> body,
       {Map<String, dynamic>? header}) {
     return dio.post(EndPoints.baseUrl + endPoint,
-        data: body, options: Options(headers: header,preserveHeaderCase: true));
+        data: body,
+        options: Options(headers: header, preserveHeaderCase: true));
   }
+
   Future<Response> delete(String endPoint, {Map<String, dynamic>? header}) {
     return dio.delete(EndPoints.baseUrl + endPoint,
         options: Options(headers: header, preserveHeaderCase: true));
   }
-  Future<Response> put(String endPoint, {Map<String, dynamic>? body,Map<String, dynamic>? header}) {
+
+  Future<Response> put(String endPoint,
+      {Map<String, dynamic>? body, Map<String, dynamic>? header}) {
     return dio.put(EndPoints.baseUrl + endPoint,
         data: body,
         options: Options(headers: header, preserveHeaderCase: true));
